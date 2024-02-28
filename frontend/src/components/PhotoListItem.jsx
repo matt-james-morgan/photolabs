6cee5b58fab3624fc2b5e6faa86ef132a29772a3
matt-james-morgan <matt.james.morgan97@gmail.com>
2024-02-28 15:01:04 -0500

    added styling to photolistitem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -7,12 +7,11 @@ import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
 
-  console.log(props);
-  
+
   const photoArray = props.data.map((user)=>{
     return (
       <li key={user.id}>
-      <img src={user.imageSource}></img>
+      <img  className="photo-list__item" src={user.imageSource}></img>
       <img src={user.profile}></img>
       <p>{user.username}</p>
       <p><span>{user.location.city}</span><span>{user.location.country}</span></p>
