1d664af303622504cf84e5724c8b8228d3937f72
matt-james-morgan <matt.james.morgan97@gmail.com>
2024-02-28 21:06:56 -0500

    adds styles to user profile

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -10,11 +10,14 @@ const PhotoListItem = (props) => {
 
   const photoArray = props.data.map((user)=>{
     return (
-      <li key={user.id}>
-      <img  className="photo-list__item" src={user.imageSource}></img>
-      <img src={user.profile}></img>
+      <li key={user.id} className="photo-list__item">
+      <img className="photo-list__img"  src={user.imageSource}></img>
+      <div className="photo-list__user-profile " >
+      <img  className="photo-list__user-info"src={user.profile}></img>
       <p>{user.username}</p>
-      <p><span>{user.location.city}</span><span>{user.location.country}</span></p>
+      <p className="photo-list__user-location"><span>{user.location.city}</span>, <span>{user.location.country}</span></p>
+      </div>
+     
       </li>
     )
   });
