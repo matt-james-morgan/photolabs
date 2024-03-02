af113c15c8a15d24987b120149379f0ee8216b4e
matt-james-morgan <matt.james.morgan97@gmail.com>
2024-03-02 10:49:31 -0500

    makes photolist container work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,14 @@
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
+import PhotoList from 'components/PhotoList';
 import './App.scss';
-
-const sampleDataForPhotoListItem = [{
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-}, {
-  id: "2",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-}, {
-  id: "3",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-}];
 
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      {sampleDataForPhotoListItem.map((photo, index) => 
-        <PhotoListItem key={index} data={photo}/>
-      )}
+      <PhotoList />
     </div>
   );
 };
