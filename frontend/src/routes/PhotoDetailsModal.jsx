258487b4c769a579a205e78b803853efbc98e4c9
matt-james-morgan <matt.james.morgan97@gmail.com>
2024-03-02 14:23:28 -0500

    fixed close button

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -6,9 +6,9 @@ import closeSymbol from '../assets/closeSymbol.svg';
 const PhotoDetailsModal = (props) => {
 
   const handleClick = () =>{
-    props.setModal(!props.modal);
+    props.setModal(false);
   }
-  
+
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={handleClick}>
