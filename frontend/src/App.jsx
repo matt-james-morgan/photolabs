536984f3bb7b46e13421b309a92e199ed940ffd6
matt-james-morgan <matt.james.morgan97@gmail.com>
2024-03-02 11:37:44 -0500

    adds homeroute to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 
-import PhotoList from 'components/PhotoList';
-import TopicList from 'components/TopicList';
-import TopNavigation from 'components/TopNavigationBar';
-import FavBadge from 'components/FavBadge';
-import './App.scss';
+import HomeRoute from 'routes/HomeRoute';
 
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <TopNavigation>
-        <TopicList />
-        <FavBadge />
-      </TopNavigation>
-      <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import TopNavigation from 'components/TopNavigationBar';
+import TopicList from 'components/TopicList';
+import FavBadge from 'components/FavBadge';
+import PhotoList from 'components/PhotoList';
 import '../styles/HomeRoute.scss';
 
 const HomeRoute = () => {
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigation>
+        <TopicList />
+        <FavBadge />
+      </TopNavigation>
+      <PhotoList />
     </div>
   );
 };
